d84264f4496ddfb63ce845d7653c47f750021d41
josdem <joseluis.delacruz@gmail.com>
2019-01-08 13:55:52 -0500

    Adding locale service and implementation

---
+++ src/main/java/com/jos/dem/spring/webflux/internationalization/service/LocaleService.java
@@ -0,0 +1,5 @@
+package com.jos.dem.spring.webflux.internationalization.service;
+
+public interface LocaleService {
+  Stirng getMessage(String code)
+}
---
+++ src/main/java/com/jos/dem/spring/webflux/internationalization/service/impl/LocaleServiceImpl.java
@@ -0,0 +1,19 @@
+package com.jos.dem.spring.webflux.internationalization.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.context.MessageSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.jos.dem.spring.webflux.internationalization.service.LocaleService;
+
+@Service
+public class LocaleServiceImpl implements LocaleService {
+
+  @Autowired
+  private MessageSource messageSource;
+
+  public String getMessage(String code){
+    return messageSource.getMessage(code, null, new Locale("en"));
+  }
+
+}
