4605e6057c7b41f62ebfaa9b1cb430c690f0ce74
josdem <joseluis.delacruz@gmail.com>
2019-01-15 16:11:09 -0500

    Adding missing templates

--- src/main/java/com/jos/dem/spring/webflux/internationalization/InternationalizationApplication.java
+++ src/main/java/com/jos/dem/spring/webflux/internationalization/InternationalizationApplication.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.spring5.ISpringWebFluxTemplateEngine;
+import org.thymeleaf.spring5.view.reactive.ThymeleafReactiveViewResolver;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 
 @SpringBootApplication
 public class InternationalizationApplication {
@@ -19,6 +25,37 @@ public class InternationalizationApplication {
     messageSource.setBasenames("languages/i18n");
     messageSource.setDefaultEncoding("UTF-8");
     return messageSource;
+  }
+
+  @Bean
+  public ITemplateResolver thymeleafTemplateResolver() {
+    final SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+    resolver.setApplicationContext(this.context);
+    resolver.setPrefix("classpath:views/");
+    resolver.setSuffix(".html");
+    resolver.setTemplateMode(TemplateMode.HTML);
+    resolver.setCacheable(false);
+    resolver.setCheckExistence(false);
+    return resolver;
+  }
+
+  @Bean
+  public ISpringWebFluxTemplateEngine thymeleafTemplateEngine() {
+    SpringWebFluxTemplateEngine templateEngine = new SpringWebFluxTemplateEngine();
+    templateEngine.setTemplateResolver(thymeleafTemplateResolver());
+    return templateEngine;
+  }
+
+  @Bean
+  public ThymeleafReactiveViewResolver thymeleafReactiveViewResolver() {
+    ThymeleafReactiveViewResolver viewResolver = new ThymeleafReactiveViewResolver();
+    viewResolver.setTemplateEngine(thymeleafTemplateEngine());
+    return viewResolver;
+  }
+
+  @Override
+  public void configureViewResolvers(ViewResolverRegistry registry) {
+    registry.viewResolver(thymeleafReactiveViewResolver());
   }
 
 }
