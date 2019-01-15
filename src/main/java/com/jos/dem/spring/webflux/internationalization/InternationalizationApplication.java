91cc92141b56deb8cdc357402b329ff501bd3486
josdem <joseluis.delacruz@gmail.com>
2019-01-14 22:00:26 -0500

    Adding MessageSource

--- src/main/java/com/jos/dem/spring/webflux/internationalization/InternationalizationApplication.java
+++ src/main/java/com/jos/dem/spring/webflux/internationalization/InternationalizationApplication.java
@@ -1,14 +1,25 @@
 package com.jos.dem.spring.webflux.internationalization;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class InternationalizationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InternationalizationApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(InternationalizationApplication.class, args);
+  }
+
+  @Bean
+  public MessageSource messageSource() {
+    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+    messageSource.setBasenames("languages/i18n");
+    messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
+  }
 
 }
 
--- src/main/java/com/jos/dem/spring/webflux/internationalization/controller/InternationalizationController.java
+++ src/main/java/com/jos/dem/spring/webflux/internationalization/controller/InternationalizationController.java
@@ -1,4 +1,15 @@
 package com.jos.dem.spring.webflux.internationalization.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.ServerRequest;
+
 @RestController
-public class InternationalizationController {}
+public class InternationalizationController {
+
+  @GetMapping("/")
+  public String index(ServerRequest request){
+    return localeService.getMessage("user.hello", request);
+  }
+
+}
