32df9bf4516beb42e30270a670895ce564bfd6b4
josdem <joseluis.delacruz@gmail.com>
2019-01-14 22:01:08 -0500

    Adding locale resolver

---
+++ src/main/java/com/jos/dem/spring/webflux/internationalization/helper/LocaleResolver.java
@@ -0,0 +1,24 @@
+package com.jos.dem.spring.webflux.internationalization.helper;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.servlet.LocaleContextResolver;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.SimpleLocaleContext;
+
+public class LocaleResolver implements LocaleContextResolver {
+
+  @Override
+  public LocaleContext resolveLocaleContext(ServerWebExchange exchange) {
+    Locale targetLocale = Locale.getDefault();
+    List<String> referLang = exchange.getRequest().getQueryParams().get("lang");
+    if (referLang != null && !referLang.isEmpty()) {
+      String lang = referLang.get(0);
+      targetLocale = Locale.forLanguageTag(lang);
+    }
+    return new SimpleLocaleContext(targetLocale);
+  }
+
+}
