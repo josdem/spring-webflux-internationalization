50e1ee4c59dcf90cc08788f1b59b05bb3e1427be
josdem <joseluis.delacruz@gmail.com>
2019-01-16 13:47:38 -0500

    Changing controller to dispatch only one webpage

--- src/main/java/com/jos/dem/spring/webflux/internationalization/controller/InternationalizationController.java
+++ src/main/java/com/jos/dem/spring/webflux/internationalization/controller/InternationalizationController.java
@@ -6,15 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class InternationalizationController {
 
-  @GetMapping("/index")
+  @GetMapping("/")
   public String index() {
     return "index";
   }
-
-  @GetMapping("/welcome")
-  public String welcome() {
-    return "welcome";
-  }
-
 
 }
