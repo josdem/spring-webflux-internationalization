2d409d600c45da357a6b5d562019a6ae0f4020ea
josdem <joseluis.delacruz@gmail.com>
2019-01-17 16:36:23 -0500

    Changing to rest controller

--- src/main/java/com/jos/dem/spring/webflux/internationalization/controller/InternationalizationController.java
+++ src/main/java/com/jos/dem/spring/webflux/internationalization/controller/InternationalizationController.java
@@ -1,9 +1,9 @@
 package com.jos.dem.spring.webflux.internationalization.handler;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class InternationalizationController {
 
   @GetMapping("/")
